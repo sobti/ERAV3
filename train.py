4cf4a2baf601158ae2fcd4b2904772d69440c090
sobti <42212648+sobti@users.noreply.github.com>
2024-12-05 12:44:55 +0000

    Update train.py

--- train.py
+++ train.py
@@ -35,7 +35,9 @@ def train_model():
       # Train Phase transformations
     train_transforms = transforms.Compose([
         transforms.ColorJitter(brightness=0.20, contrast=0.1, saturation=0.10, hue=0.1),
-        transforms.RandomRotation((-10, 10), fill=(0,)),
+        transforms.RandomRotation((-30, 30), fill=(0,)),
+        transforms..RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))  # Mean and std are tuples
     ])
